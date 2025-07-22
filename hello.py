0b750b39513bedaf52d5138aedd6a4bed57f3ad6
nicokarachev <nicokarachev.dev@gmail.com>
2025-07-22 07:08:45 +0000

    Python uses indentation to indicate a block of code

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ import sys
 print(sys.version)
 
 print("hello, world!")
+
+#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+if 5 > 2:
+  print("Five is greater than two!")
