2fedf4bcbe8dc5a9aa6485069c17d255c5b42d07
nicokarachev <nicokarachev.dev@gmail.com>
2025-07-22 07:10:51 +0000

    Python has no command for declaring a variable.

--- hello.py
+++ hello.py
@@ -7,3 +7,9 @@ print("hello, world!")
 #The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
 if 5 > 2:
   print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
