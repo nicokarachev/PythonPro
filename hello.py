17ae105d6a7482a0251f9c9fc148b54875f1f916
nicokarachev <nicokarachev.dev@gmail.com>
2025-07-22 07:17:29 +0000

    finshed variable

--- hello.py
+++ hello.py
@@ -19,3 +19,37 @@ This is a comment
 written in
 more than just one line
 """
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+print(x, y, z)
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
